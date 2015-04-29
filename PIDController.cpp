1fd74af70a6dddd8af28318e3a773a5077d1810c
Nick Schulz <nschulz@spsu.edu>
2015-04-29 18:08:04 +0000

    Attempt to fix tabs
    GitHub was displaying some tabs differently

--- PIDController.cpp
+++ PIDController.cpp
@@ -10,77 +10,77 @@
 using namespace boost::chrono;
 
 PIDController::PIDController(double kp, double ki, double kd) {
-    this->setGains(kp, ki, kd);
-    this->setConstraints(-1, -1);
-    this->init();
+	this->setGains(kp, ki, kd);
+	this->setConstraints(-1, -1);
+	this->init();
 }
 
 PIDController::PIDController(double kp, double ki, double kd, double lowerConstraint, double upperConstraint) {
-    this->setGains(kp, ki, kd);
-    this->setConstraints(lowerConstraint, upperConstraint);
-    this->init();
+	this->setGains(kp, ki, kd);
+	this->setConstraints(lowerConstraint, upperConstraint);
+	this->init();
 }
 
 PIDController::PIDController() {
-    this->setGains(0, 0, 0);
-    this->setConstraints(-100,100);
-    this->init();
+	this->setGains(0, 0, 0);
+	this->setConstraints(-100,100);
+	this->init();
 }
 
 PIDController::PIDController(const PIDController& orig) {
-    this->setGains(orig.kp, orig.ki, orig.kd);
-    this->setConstraints(orig.lowerConstraint, orig.upperConstraint);
-    integrator = orig.integrator;
-    lastError = orig.lastError;
+	this->setGains(orig.kp, orig.ki, orig.kd);
+	this->setConstraints(orig.lowerConstraint, orig.upperConstraint);
+	integrator = orig.integrator;
+	lastError = orig.lastError;
 }
 
 PIDController::~PIDController() {
 }
-        
+		
 void PIDController::targetSetpoint(double setpoint) {
-    this->setpoint = setpoint;
-    peakTime = -1;
-	settlingTime = -1;
-	percentOvershoot = 0;
-    sample_timer.start();
-    performance_timer.start();
-}
-
-void PIDController::setGains(double kp, double ki, double kd) {
-    this->kp = kp;
-    this->ki = ki;
-    this->kd = kd;
-}
-
-void PIDController::setConstraints(double lowerConstraint, double upperConstraint) {
-    this->lowerConstraint = lowerConstraint;
-    this->upperConstraint = upperConstraint;
-}
-
-double PIDController::getSetpoint() {
-    return setpoint;
-}
-
-double PIDController::getKp() {
-    return kp;
-}
-
-double PIDController::getKi() {
-    return ki;
-}
-
-double PIDController::getKd() {
-    return kd;
-}
-
-void PIDController::init() {
-    setpoint = 0;
-    integrator = 0;
-    lastError = 0;
+	this->setpoint = setpoint;
 	peakTime = -1;
 	settlingTime = -1;
 	percentOvershoot = 0;
-    sample_timer.stop();
+	sample_timer.start();
+	performance_timer.start();
+}
+
+void PIDController::setGains(double kp, double ki, double kd) {
+	this->kp = kp;
+	this->ki = ki;
+	this->kd = kd;
+}
+
+void PIDController::setConstraints(double lowerConstraint, double upperConstraint) {
+	this->lowerConstraint = lowerConstraint;
+	this->upperConstraint = upperConstraint;
+}
+
+double PIDController::getSetpoint() {
+	return setpoint;
+}
+
+double PIDController::getKp() {
+	return kp;
+}
+
+double PIDController::getKi() {
+	return ki;
+}
+
+double PIDController::getKd() {
+	return kd;
+}
+
+void PIDController::init() {
+	setpoint = 0;
+	integrator = 0;
+	lastError = 0;
+	peakTime = -1;
+	settlingTime = -1;
+	percentOvershoot = 0;
+	sample_timer.stop();
 	performance_timer.stop();   
 }
 
@@ -95,7 +95,7 @@ bool PIDController::hasSettled() {
 }
 
 double PIDController::calc(double processVariable) {
-    sample_timer.stop();
+	sample_timer.stop();
 	
 	double percent = (processVariable/setpoint) - 1;
 	
@@ -114,20 +114,20 @@ double PIDController::calc(double processVariable) {
 	}
 	
 	double error = setpoint - processVariable;
-    double samplingTime = (sample_timer.elapsed().wall)/1e9;
+	double samplingTime = (sample_timer.elapsed().wall)/1e9;
 	double differentiator = (error - lastError)/samplingTime;
-    integrator += (error * samplingTime);
-    double controlVariable = kp * error + ki * integrator + kd * differentiator;
-    if(controlVariable < lowerConstraint) {
-        controlVariable = lowerConstraint;
-    }
-    else if(controlVariable > upperConstraint) {
-        controlVariable = upperConstraint;
-    }
-    lastError = error;
+	integrator += (error * samplingTime);
+	double controlVariable = kp * error + ki * integrator + kd * differentiator;
+	if(controlVariable < lowerConstraint) {
+		controlVariable = lowerConstraint;
+	}
+	else if(controlVariable > upperConstraint) {
+		controlVariable = upperConstraint;
+	}
+	lastError = error;
 
 	sample_timer.start();
 
-    return controlVariable;
+	return controlVariable;
 }
 
